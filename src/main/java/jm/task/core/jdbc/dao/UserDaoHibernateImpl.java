05c3388f4f5141e3abf21a177ee0e15bd2a4dbf0
bobis <bobishoev.0507@gmail.com>
2022-04-04 14:31:30 +0300

    jdbs

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,126 +3,72 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    private final SessionFactory sessionFactory = Util.getConnection();
-
+    private Session session;
     public UserDaoHibernateImpl() {
 
     }
 
     @Override
     public void createUsersTable() {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        try {
-            session.createNativeQuery("CREATE TABLE IF NOT EXISTS mybase.users" +
-                    " (id mediumint not null auto_increment, name VARCHAR(50), " +
-                    "lastname VARCHAR(50), " +
-                    "age tinyint, " +
-                    "PRIMARY KEY (id))").executeUpdate();
-            transaction.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-            if (transaction != null) {
-                transaction.rollback();
-            }
-        } finally {
-            session.close();
-        }
+
+       session = Util.getConnection().openSession();
+       session.beginTransaction();
+       session.createSQLQuery( "CREATE TABLE IF NOT EXISTS mybase.users" +
+               " (id mediumint not null auto_increment, name VARCHAR(50), " +
+                       "lastname VARCHAR(50), " +
+                       "age tinyint, " +
+                       "PRIMARY KEY (id));").executeUpdate();
+       session.getTransaction().commit();
+       session.close();
     }
 
     @Override
     public void dropUsersTable() {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        try {
-            session.createNativeQuery("DROP TABLE IF EXISTS mybase.users").executeUpdate();
-            transaction.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-            if (transaction != null) {
-                transaction.rollback();
-            }
-        } finally {
-            session.close();
-        }
+        session = Util.getConnection().openSession();
+        session.beginTransaction();
+        session.createSQLQuery("DROP TABLE IF EXISTS mybase.users").executeUpdate();
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        try {
-            session.save(new User(name, lastName, age));
-            transaction.commit();
-            System.out.println("User с именем – " + name + " добавлен в базу данных");
-        } catch (Exception e) {
-            e.printStackTrace();
-            if (transaction != null) {
-                transaction.rollback();
-            }
-        } finally {
-            session.close();
-        }
+        session = Util.getConnection().openSession();
+        session.beginTransaction();
+        session.save(new User(name, lastName, age));
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
     public void removeUserById(long id) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        try {
-            session.delete(session.get(User.class, id));
-            transaction.commit();
-            System.out.println("User удален");
-        } catch (Exception e) {
-            e.printStackTrace();
-            if (transaction != null) {
-                transaction.rollback();
-            }
-        } finally {
-            session.close();
-        }
+        session = Util.getConnection().openSession();
+        session.beginTransaction();
+        session.delete(session.get(User.class, id));
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<User> getAllUsers() {
-        Session session = sessionFactory.openSession();
-        CriteriaQuery<User> criteriaQuery = session.getCriteriaBuilder().createQuery(User.class);
-        criteriaQuery.from(User.class);
-        Transaction transaction = session.beginTransaction();
-        List<User> userList = session.createQuery(criteriaQuery).getResultList();
-        try {
-            transaction.commit();
-            return userList;
-        } catch (Exception e) {
-            e.printStackTrace();
-            transaction.rollback();
-        } finally {
-            session.close();
-        }
-        return userList;
+        session = Util.getConnection().openSession();
+        session.beginTransaction();
+        List <User> listUser = session.createQuery("from User").list();
+        session.close();
+        return listUser;
     }
 
     @Override
     public void cleanUsersTable() {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        try {
-            session.createNativeQuery("TRUNCATE TABLE mybase.users;").executeUpdate();
-            transaction.commit();
-            System.out.println("Таблица очищена");
-        } catch (Exception e) {
-            e.printStackTrace();
-            if (transaction != null) {
-                transaction.rollback();
-            }
-        } finally {
-            session.close();
-        }
+        session = Util.getConnection().openSession();
+        session.beginTransaction();
+        session.createQuery("delete from User").executeUpdate();
+        session.getTransaction().commit();
+        session.close();
+
     }
 }
