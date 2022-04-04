6eee9360b29786510cc64bd6da08da5e34a9ed54
bobis <bobishoev.0507@gmail.com>
2022-04-04 14:31:23 +0300

    jdbs

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package jm.task.core.jdbc.service;
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 
 import jm.task.core.jdbc.model.User;
+
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDaoHibernateImpl userDao = new UserDaoHibernateImpl();
+    UserDao userDao = new UserDaoHibernateImpl();
     @Override
     public void createUsersTable() {
         userDao.createUsersTable();
@@ -15,16 +18,16 @@ public class UserServiceImpl implements UserService {
         userDao.dropUsersTable();
     }
 
-    public void saveUser(String name, String lastName, byte age)  {
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
         userDao.saveUser(name, lastName, age);
 
     }
 
-    public void removeUserById(long id)  {
+    public void removeUserById(long id) throws SQLException {
         userDao.removeUserById(id);
     }
 
-    public List<User> getAllUsers() {
+    public List<User> getAllUsers() throws SQLException {
         List<User> users =  userDao.getAllUsers();
         for (User user : users) {
             System.out.println(user);
